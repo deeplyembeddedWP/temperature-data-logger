ce34813b4f4480ae502f7247f879ec647c48f77c
Vinay Divakar <deeplyembeddedWP@users.noreply.github.com>
2024-06-30 09:54:50 +1200

    Sensor_Timperature: added functions to get average temperature data, fixed minor issues

--- temperature logger/temperature logger/user_src/subsystems/sensors/Sensors_Temperature.c
+++ temperature logger/temperature logger/user_src/subsystems/sensors/Sensors_Temperature.c
@@ -2,26 +2,20 @@
  * Temperature_Sensor.c
  *
  * Created: 29/06/2024 6:15:10 pm
- *  Author: Vinay Divakar
+ * Author: Vinay Divakar
+ * Brief: subsystem to interact with an array of temperature sensors
  */
-#include "TMP117.h"
-#include "hal_delay.h"
 
-#define SENSORS_TEMPERATURE_SUPPORTED_MAX 4
+#include "Sensors_Temperature.h"
 
-/*
-sensor operating states
-*/
-enum sensors_status_t {
-  SENSORS_TEMPERATURE_OK = 0,
-  SENSORS_TEMPERATURE_FAULTY = -1,
-  SENSORS_TEMPERATURE_ERRATIC = -2,
-};
+// max number of channels supported
+#define SENSORS_TEMPERATURE_CHANNELS_NUM_MAX 4
+#define SENSORS_TEMPERATURE_VARIANCE_PERCENTAGE_MAX 5
 
 /*
-populate the structure for the intended configuration value
+populate the structure for the intended configuration
 */
-static TMP117_configuration_register_t configuration = {};
+static TMP117_configuration_register_t _configuration = {};
 
 /*
 structure to hold the state of the sensor
@@ -33,12 +27,12 @@ struct sensor_temperature_t {
 };
 
 /*
-context to manage interactions with all the sensors
+context holding the sensor list to manage interactions with all the sensors
 */
 static struct sensors_temperature_context_t {
   struct sensor_temperature_t
-      sensors[SENSORS_TEMPERATURE_SUPPORTED_MAX]; // holds data unique to each
-                                                  // sensor
+      sensors[SENSORS_TEMPERATURE_CHANNELS_NUM_MAX]; // holds data unique to
+                                                     // each sensor
   enum sensors_status_t status; // indicates the overall health of the sensors
 } _this = {
     .sensors[0].address = TMP117_ADDRESS_0,
@@ -50,10 +44,8 @@ static struct sensors_temperature_context_t {
 /**
  * @brief scans the bus and initializes the reachable devices
  */
-void Sensors_Temperature_ports_initialize(void) {
-  delay_ms(TMP117_POWER_ON_TIME_MS);
-  for (size_t i = 0; i < sizeof(_this.sensors) / sizeof(_this.sensors[0]);
-       i++) {
+static void _ports_scan(void) {
+  for (size_t i = 0; i < SENSORS_TEMPERATURE_CHANNELS_NUM_MAX; i++) {
     _this.sensors[i].initialized =
         TMP117_device_id_status_get(_this.sensors[i].address);
   }
@@ -64,20 +56,101 @@ void Sensors_Temperature_ports_initialize(void) {
  *
  * @return 0 success, <0 if one or more sensors failed to configure
  */
-int Sensors_Temperature_configure(void) {
+static int _configure(void) {
   int err = 0;
-  for (size_t i = 0; i < sizeof(_this.sensors) / sizeof(_this.sensors[0]);
-       i++) {
-    if (!_this.sensors[i].initialized)
+  for (size_t i = 0; i < SENSORS_TEMPERATURE_CHANNELS_NUM_MAX; i++) {
+    if (!_this.sensors[i].initialized) {
       continue;
-
-    err = TMP117_configure(_this.sensors[i].address, configuration.value);
+    }
+    // try configuring the sensor
+    err = TMP117_configure(_this.sensors[i].address, _configuration.value);
     if (err) { // check for errors
-      _this.status = SENSORS_TEMPERATURE_FAULTY;
-      // somethings not right, sensor could be unreliable, set it to false so we
-      // attempt reading from it
+      _this.status = SENSORS_TEMPERATURE_CONFIG_ERROR;
+      // somethings not right, sensor could be unreliable, set it to false
+      // so we don't attempt reading from it. Will a retrying make it any
+      // better?
       _this.sensors[i].initialized = false;
     }
   }
   return _this.status;
+}
+
+/**
+ * @brief calculates variance using the current measurements and
+ * checks how spread out the measurements of each sensor is from the mean.
+
+ * @return +/- percentage of spread
+ */
+static int _temperature_variance_check(void) {
+  // TODO: some math here, pretty straightforward
+  return 0;
+}
+
+/**
+ * @brief scan ports and initialize the reachable sensors
+ */
+void Sensors_Temperature_initialize(void) {
+  delay_ms(TMP117_POWER_ON_TIME_MS);
+  _ports_scan();
+  _configure();
+}
+
+/**
+ * @brief gets the most recent status of the sensors
+ *
+ * @return status
+ */
+int Sensors_Temperature_subsys_status_get(void) { 
+	return _this.status; 
+}
+
+/**
+ * @brief iterates the sensor list, grabs measurements from each sensor
+ * if available, computes and returns the average of all successful readings.
+ * retrieves average temperature reading from all the sensors.
+ *
+ * @return average temperature reading, 0.0 if none was a success
+ */
+float Sensors_Temperature_reading_get(void) {
+  int status = 0;
+  float value = 0.0;
+
+  // status shall be overridden if any errors occur only if there existed no
+  // config errors
+  _this.status = (_this.status == SENSORS_TEMPERATURE_CONFIG_ERROR)
+                     ? _this.status
+                     : SENSORS_TEMPERATURE_OK;
+
+  for (size_t i = 0; i < SENSORS_TEMPERATURE_CHANNELS_NUM_MAX; i++) {
+    if (!_this.sensors[i].initialized) {
+      continue;
+    }
+    _this.sensors[i].temperature = 0.0; // reset reading
+
+    status = TMP117_data_ready_status_get(_this.sensors[i].address);
+    if (!status) { // data unavailable
+      continue;
+    } else if (status == true) { // data is available
+      value = TMP117_temperature_celcius_get(_this.sensors[i].address);
+    }
+
+    // check for errors
+    if (status == ERR_INVALID_DATA || (int)value == ERR_INVALID_DATA) {
+      // somethings not right, either conversion was incomplete or measurement
+      // failed for this sensor, set the fault code to indicate this error
+      _this.status = SENSORS_TEMPERATURE_FAULTY;
+    } else { // no errors
+      _this.sensors[i].temperature = value;
+    }
+  }
+
+  // calculate variance and update status if out of tolerance band
+  _temperature_variance_check();
+
+  // compute and return the average
+  for (size_t i = 0; i < SENSORS_TEMPERATURE_CHANNELS_NUM_MAX; i++) {
+    value += _this.sensors[i].temperature;
+  }
+
+  return (value / SENSORS_TEMPERATURE_CHANNELS_NUM_MAX);
 }
--- temperature logger/temperature logger/user_src/subsystems/sensors/Sensors_Temperature.h
+++ temperature logger/temperature logger/user_src/subsystems/sensors/Sensors_Temperature.h
@@ -2,14 +2,29 @@
  * Sensors_Temperature.h
  *
  * Created: 29/06/2024 6:12:57 pm
- *  Author: Vinay Divakar
+ * Author: Vinay Divakar
+ * Brief: subsystem to interact with an array of temperature sensors
  */ 
 
 
 #ifndef SENSORS_TEMPERATURE_H_
 #define SENSORS_TEMPERATURE_H_
 
-void Sensors_Temperature_ports_initialize(void);
+#include "TMP117.h"
+#include "hal_delay.h"
 
+/*
+sensor operating states
+*/
+enum sensors_status_t {
+	SENSORS_TEMPERATURE_OK = 0,
+	SENSORS_TEMPERATURE_CONFIG_ERROR = -1,
+	SENSORS_TEMPERATURE_FAULTY = -1,
+	SENSORS_TEMPERATURE_ERRATIC = -2,
+};
+
+void Sensors_Temperature_initialize(void);
+float Sensors_Temperature_reading_get(void);
+int Sensors_Temperature_subsys_status_get(void);
 
 #endif /* SENSORS_TEMPERATURE_H_ */
