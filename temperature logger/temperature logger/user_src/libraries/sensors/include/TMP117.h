e2d0b097b6921992805a4d587fc7c4dcaed2f833
Vinay Divakar <deeplyembeddedWP@users.noreply.github.com>
2024-06-29 18:03:43 +1200

    TMP117: refactored and tidied up the lib

--- temperature logger/temperature logger/user_src/libraries/sensors/include/TMP117.h
+++ temperature logger/temperature logger/user_src/libraries/sensors/include/TMP117.h
@@ -2,54 +2,49 @@
  * TMP117.h
  *
  * Created: 29/06/2024 1:42:42 pm
- *  Author: Vinay Divakar
- */ 
+ * Author: Vinay Divakar
+ * Brief: minimal lib to interact with the sensor
+ */
+#include "I2C.h"
+#include "err_codes.h"
+#include "stdio_start.h"
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifndef TMP117_H_
 #define TMP117_H_
 
-#define TMP117_ADDRESS_0 0x48 //ADD0 connected to GND
-#define TMP117_ADDRESS_1 0x49 //ADD0 connected to Vcc
-#define TMP117_ADDRESS_2 0x4A //ADD0 connected to SDA
-#define TMP117_ADDRESS_3 0x4B //ADD0 connected to SCL
-
-/*
-Pointer Registers
-*/
-#define TMP117_ID_REGISTER_ADDRESS 0x0F
-#define TMP117_CONFIG_REGISTER_ADDRESS 0x01
-#define TMP117_TEMPERATURE_REGISTER_ADDRESS	0x00
-
-#define TMP117_DEVICE_ID_VALUE_EXPECTED 0x0117 // Value found in the device ID register on reset 
-#define TMP117_SUPPORTED_RESOLUTION 0.0078125f // Resolution of the device
+#define TMP117_ADDRESS_0 0x48 // ADD0 connected to GND
+#define TMP117_ADDRESS_1 0x49 // ADD0 connected to Vcc
+#define TMP117_ADDRESS_2 0x4A // ADD0 connected to SDA
+#define TMP117_ADDRESS_3 0x4B // ADD0 connected to SCL
 
 // Configuration register found on page 25 Figure 26 and Table 6
 typedef union {
-	struct{
-		uint8_t unused : 1;		// Empty bit in register
-		uint8_t soft_reset : 1; // Software reset bit
-		uint8_t dr_alert : 1;		// ALERT pin select bit
-		uint8_t pol : 1;			// ALERT pin polarity bit
-		uint8_t t_na : 1;			// Therm/alert mode select
-		uint8_t avg : 2;			// Conversion averaging modes
-		uint8_t conv : 3;			// Conversion cycle bit
-		uint8_t mod : 2;			// Set conversion mode
-		uint8_t eeprom_busy : 1;	// EEPROM busy flag
-		uint8_t data_ready : 1;		// Data ready flag
-		uint8_t low_alert : 1;		// Low Alert flag
-		uint8_t high_alert : 1;		// High Alert flag
-	} configuration_feilds;
-	uint16_t configuration_register;
+  struct {
+    uint8_t unused : 1;      // Empty bit in register
+    uint8_t soft_reset : 1;  // Software reset bit
+    uint8_t dr_alert : 1;    // ALERT pin select bit
+    uint8_t pol : 1;         // ALERT pin polarity bit
+    uint8_t t_na : 1;        // Therm/alert mode select
+    uint8_t avg : 2;         // Conversion averaging modes
+    uint8_t conv : 3;        // Conversion cycle bit
+    uint8_t mod : 2;         // Set conversion mode
+    uint8_t eeprom_busy : 1; // EEPROM busy flag
+    uint8_t data_ready : 1;  // Data ready flag
+    uint8_t low_alert : 1;   // Low Alert flag
+    uint8_t high_alert : 1;  // High Alert flag
+  } configuration_feilds;
+  uint16_t configuration_register;
 } TMP117_configuration_register_t;
 
 // Device ID Register used for checking if the device ID is the same as declared
 typedef union {
-	struct{
-		uint16_t did : 12; // Indicates the device ID
-		uint8_t rev : 4;   // Indicates the revision number
-	} device_id_fields;
-	uint16_t device_id_register;
+  struct {
+    uint16_t did : 12; // Indicates the device ID
+    uint8_t rev : 4;   // Indicates the revision number
+  } device_id_fields;
+  uint16_t device_id_register;
 } TMP117_device_id_register_t;
 
 #endif /* TMP117_H_ */
--- temperature logger/temperature logger/user_src/libraries/sensors/src/TMP117.c
+++ temperature logger/temperature logger/user_src/libraries/sensors/src/TMP117.c
@@ -2,22 +2,130 @@
  * TMP117.c
  *
  * Created: 29/06/2024 1:43:06 pm
- *  Author: Vinay Divakar
+ * Author: Vinay Divakar
+ * Brief: minimal lib to interact with the sensor
  */
-#include "I2C.h"
 #include "TMP117.h"
+
+/*
+Pointer Registers
+*/
+#define TMP117_ID_REGISTER_ADDRESS 0x0F
+#define TMP117_CONFIG_REGISTER_ADDRESS 0x01
+#define TMP117_TEMPERATURE_REGISTER_ADDRESS 0x00
+
+/*
+Expected device ID and supported resolution
+*/
+#define TMP117_DEVICE_ID_VALUE_EXPECTED 0x0117
+#define TMP117_SUPPORTED_RESOLUTION 0.0078125f
+
+/*
+Data ready bit in the configuration register
+*/
+#define TMP117_DATA_READY_BIT_POS 13
+#define TMP117_DATA_READY_BIT (1U << TMP117_DATA_READY_BIT_POS)
+
+/**
+ * @brief writes a value to a register
+ *
+ * @param[in] slave_addr slave address to read from
+ * @param[in] register_addr register address to be written to
+ * @param[in] value value to be written to register_addr
+ * @return 0 success, <0 error
+ */
+static int _register_write(int16_t slave_addr, uint8_t register_addr,
+                           uint16_t value) {
+  uint8_t commmand[2] = {((value & 0xFF00) >> 8), (value & 0x00FF)};
+
+  int err = I2C_master_write(slave_addr, register_addr, commmand,
+                             sizeof(commmand) / sizeof(commmand[0]));
+  if (err) { // check for errors
+    printf("%s: sl_addr:0x%02X r_addr:0x%02X val:0x%04X err:%d\r\n",
+           __FUNCTION__, slave_addr, register_addr, value, err);
+  }
+
+  return err;
+}
+
+/**
+ * @brief reads data from a register
+ *
+ * @param[in] slave_addr slave address to read from
+ * @param[in] register_addr address to read from
+ * @return data on success, UINT16_MAX on error
+ */
+static uint16_t _register_read(int16_t slave_addr, uint8_t register_addr) {
+  uint8_t result[2] = {};
+
+  int err = I2C_master_read(slave_addr, register_addr, result,
+                            sizeof(result) / sizeof(result[0]));
+  if (err) { // check for errors
+    printf("%s: sl_addr:0x%02X r_addr:0x%02X err:%d\r\n", __FUNCTION__,
+           slave_addr, register_addr, err);
+    return UINT16_MAX;
+  }
+
+  // grab the data starting from ms-byte
+  return ((result[0] << 8) | result[1]);
+}
 
 /**
  * @brief configures the sensors functions
-
+ *
  * @param[in] slave_addr slave address to read from
  * @param[in] config_value value to be written to the configuration register
  * @return 0 success, <0 error
  */
 int TMP117_configure(int16_t slave_addr, uint16_t config_value) {
-  uint8_t commmand[2] = {((config_value & 0xFF00) >> 8),
-                         (config_value & 0x00FF)};
+  return _register_write(slave_addr, TMP117_CONFIG_REGISTER_ADDRESS,
+                         config_value);
+}
 
-  return I2C_master_write(slave_addr, TMP117_CONFIG_REGISTER_ADDRESS, commmand,
-                          sizeof(commmand) / sizeof(commmand[0]));
+/**
+ * @brief retrieves device id from the sensor
+ *
+ * @param[in] slave_addr slave address to read from
+ * @return device id success, ERR_INVALID_DATA on error
+ */
+uint16_t TMP117_device_id_get(int16_t slave_addr) {
+  uint16_t id = _register_read(slave_addr, TMP117_ID_REGISTER_ADDRESS);
+  if (id == UINT16_MAX || id != TMP117_DEVICE_ID_VALUE_EXPECTED) { // verify id
+    return ERR_INVALID_DATA;
+  }
+
+  return id;
+}
+
+/**
+ * @brief retrieves data ready status
+ *
+ * @param[in] slave_addr slave address to read from
+ * @return true on ready, false on not ready, ERR_INVALID_DATA on error
+ */
+bool TMP117_data_ready_status_get(int16_t slave_addr) {
+  uint16_t data_ready =
+      _register_read(slave_addr, TMP117_CONFIG_REGISTER_ADDRESS);
+  if (data_ready == UINT16_MAX) {
+    return ERR_INVALID_DATA;
+  }
+
+  return (data_ready & TMP117_DATA_READY_BIT);
+}
+
+/**
+ * @brief retrieves temperature in degrees celceus
+ *
+ * @param[in] slave_addr slave address to read from
+ * @return data on success, ERR_INVALID_DATA on error
+ */
+float TMP117_temperature_celcius_get(int16_t slave_addr) {
+  uint16_t temperature =
+      _register_read(slave_addr, TMP117_TEMPERATURE_REGISTER_ADDRESS);
+  if (temperature == UINT16_MAX) {
+    return ERR_INVALID_DATA;
+  }
+
+  // multiply by resolution to get the temperature in degree celceus
+  return ((int16_t)temperature * TMP117_SUPPORTED_RESOLUTION);
 }
